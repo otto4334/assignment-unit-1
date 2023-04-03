6eae706597138af88b6337f3751c68ef4cf5b29b
Michael Otto <otto4334@gmail.com>
2023-04-03 18:54:12 -0500

    answered q's

---
+++ assignment/main.js
@@ -0,0 +1,76 @@
+// console.log('Hello, World!');
+
+// let greeting = 'Hello, World!';
+// let children =2 + 1;
+// let hadDinner = false;
+
+// console.log(hadDinner);
+
+// hadDinner = true;
+
+// console.log(hadDinner);
+
+// let nuthin = null;
+// let alsoNuthin = undefined;
+
+// //used for commenting 
+
+// children = children + 1;
+// console.log(children);
+
+// children += 1;
+// console.log(children);
+
+// children++;
+// console.log(children);
+// //++ adds 1
+
+// children /= 5;
+// console.log(children);
+
+// children *= 3;
+// console.log(children);
+
+// children -= 1;
+// console.log(children);
+
+
+// console.log(children % 2);
+
+// console.log('Java' + 'Script');
+
+// console.log(greeting + 'For Realz');
+// // + is overloaded javascript wants to make it right
+// //example
+// console.log(children - 'For Realz');
+
+// console.log('Children:', children);
+
+// console.log(document.querySelector('#subtitle'))
+
+// let subtitle = document.querySelector('#subtitle')
+// subtitle.innerHTML = 'I love javaScript';
+
+    let children = 4;
+    //change this ^ to a higher or lower number for changed
+    // else fires if if does not
+    if (children > 6) {
+        console.log('I have partial kids!')
+    } else if (children > 3) {
+        console.log('I have a medium number of kids')
+    } else {
+        console.log('I do not have any kids');
+    }    
+
+    let ducks = 119
+
+    if (ducks > 80 && ducks < 120) {
+        console.log('perfect number of ducks');
+    }
+    //|| is or
+
+    let name = 'Sue'
+
+    if (name === 'Sue' || name === 'Bob') {
+        console.log('Hello friend')
+    }
